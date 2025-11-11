198b22ff507ced08245893f1c8ef8f8927567eed
Conor Restall <conor@restall.io>
2025-11-11 19:39:52 +0000

    add tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    safelist: [
+        'h-3', 'h-4', 'h-5'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
